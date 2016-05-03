01004f6725a15d4901fb2af2acf576bcb6db411c
kaworurei <349443986@qq.com>
2016-05-03 17:27:03 +0800

    Initial commit

---
+++ src/com/j2eedemo/controller/HelloWorldController.java
@@ -0,0 +1,53 @@
+package com.j2eedemo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Administrator on 2016/5/3.
+ */
+@Controller
+@RequestMapping()
+public class HelloWorldController  {
+
+    @RequestMapping(value ="/index",method = {RequestMethod.GET})
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("message", "Hello World!");
+        modelAndView.setViewName("start");
+        return modelAndView;
+    }
+
+    @RequestMapping(value ="/detail/{id}",method = {RequestMethod.GET})
+    public ModelAndView getDetail(@PathVariable(value = "id") Integer id){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("message", String.valueOf(id));
+        modelAndView.setViewName("start");
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/*", method = {RequestMethod.GET})
+    public ModelAndView urlTest(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("message", "Uncatch");
+        modelAndView.setViewName("start");
+        return modelAndView;
+    }
+//
+//    @RequestParam(value="username", required=true, defaultValue="AAA")。
+    @RequestMapping(value="/param", params="param", method = {RequestMethod.GET})
+    public ModelAndView paramsTest(HttpServletRequest request, @RequestParam("param") String param){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("message", param+" "+ ServletRequestUtils.getStringParameter(request, "param", null));
+        modelAndView.setViewName("start");
+        return modelAndView;
+    }
+}
